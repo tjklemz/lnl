ac58aaf0ae7858d55c656c36c27f04c56a79fe78
Thomas Klemz <tjklemz@gmail.com>
2015-07-18 12:04:18 -0700

    animation change

--- js/main.js
+++ js/main.js
@@ -1,23 +1,58 @@
 ;(function() {
   
-  var logoString = "Lory&Ludlow";
+  var logoString = "Lory&Ludlow",
+      len = logoString.length;
   
-  function fadeLetter(className) {
-    document.getElementsByClassName(className)[0].classList.add('fade');
+  function fadeLetter(letter) {
+    letter.classList.remove('show');
+    letter.classList.add('fade');
   }
   
-  setTimeout(function() {
-    var len, i;
+  function forEachLetter(visit) {
+    var i;
     
-    for (len = logoString.length, i = 2; i <= len; ++i) {
-      var delay = 1500*Math.exp(-i/10);
-      var letter = 'lnl-letter-' + i;
-      setTimeout(fadeLetter.bind(null, letter), delay);
+    for (i = 2; i <= len; ++i) {
+      var className = 'lnl-letter-' + i;
+      var letter = document.getElementsByClassName(className)[0];
+      visit(letter, i, len);
     }
-  }, 600);
+  }
   
-  setTimeout(function() {
-    document.querySelector('.logo-square').classList.add('show');
-  }, 2000);
+  document.addEventListener('DOMContentLoaded', function() {
+  
+    setTimeout(function() {
+      var delay = 0;
+    
+      forEachLetter(function(letter, i, total) {
+        delay = 0.88*Math.exp(i/2) + 200;
+        console.log(delay);
+        setTimeout(function() {
+          letter.classList.add('show');
+        }, delay);
+      });
+    
+      delay += 800;
+    
+      setTimeout(function() {
+        forEachLetter(fadeLetter);
+        console.log('hi');
+      }, delay);
+    
+      delay += 100;
+    
+      console.log(delay);
+    
+      setTimeout(function() {
+        document.querySelector('.logo-square').classList.add('show');
+      }, delay);
+    }, 600);
+  });
+  
+  // setTimeout(function() {
+  //   forEachLetter(function(letter, i, total) {
+  //     var delay = 1500*Math.exp(-i/10);
+  //     setTimeout(fadeLetter.bind(null, letter), delay);
+  //   });
+  // }, 600);
   
 })();
